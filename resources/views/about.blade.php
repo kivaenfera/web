4e475d648fd8574273363457b9b47bd715d280f8
kivaenfera <2121200477@soy.utj.edu.mx>
2022-11-20 15:53:09 -0600

    Subida de archivos a github

---
+++ resources/views/about.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Netline</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader --> 
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="head_top">
+               <div class="container">
+                  <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <ul class="sociel_link">
+                         <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-linkedin"></i></a></li>
+                     </ul>
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <p>long established fact that a reader will be </p>
+                    </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="index.html"><img src="images/logo.PNG" alt="logo"/></a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-7 col-lg-7 col-md-9 col-sm-9">
+                  <div class="menu-area">
+                     <div class="limit-box">
+                        <nav class="main-menu">
+                           <ul class="menu-area-main">
+                              <li> <a href="index.html">Home</a> </li>
+                              <li class="active"> <a href="about.html">About us</a> </li>
+                              <li> <a href="product.html">packages</a> </li>
+                              <li> <a href="blog.html"> solution</a> </li>
+                              <li> <a href="contact.html">Contact</a> </li>
+                              <li class="mean-last"> <a href="#contact">signup</a> </li>
+                               
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                  <li><a class="buy" href="#">Login</a></li>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner --> 
+      </header>
+      <!-- end header -->
+       <div class="brand_color">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage">
+                        <h2>about us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+<div class="about">
+   <div class="container">
+      <div class="row">
+         <dir class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="about_box">
+               <figure><img src="images/pc_layout.png"/></figure>
+            </div>
+         </dir>
+          <dir class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="about_box">
+               <h3>At Netline we provide professional service and personalized attention for each sector</h3>
+
+               <p>ABOUT US!</p>
+
+               
+               <p>We are a professional and certified team, specialized in providing solutions in communication and technology with tailor-made products and services of excellent quality.</p>
+               <p>We have the experience to help our clients from different sectors such as home, industry and business, providing the best proposals to reduce costs and accelerate return on investment.</p>
+            </div>
+         </dir> 
+      </div>
+   </div>
+</div>
+
+<!-- CHOOSE  -->
+      <div class="whyschose">
+         <div class="container">
+
+            <div class="row">
+               <div class="col-md-7 offset-md-3">
+                  <div class="title">
+                     <h2>Why <strong class="black">choose us</strong></h2>
+                     <span>Fastest repair service with best price!</span>
+                     <span>We will make your experience rewarding and of quality!</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="choose_bg">
+         <div class="container">
+            <div class="white_bg">
+            <div class="row">
+               <dir class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="for_box">
+                     <i><img src="icon/1.png"/></i>
+                     <h3>SWITCHES, ROUTERS AND PBX</h3>
+                     <p>Active wiring equipment to interconnect computers and mobile devices with cable and WIFI, always keeping your networks secure.</p>
+                  </div>
+               </dir>              
+               <dir class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="for_box">
+                     <i><img src="icon/3.png"/></i>
+                     <h3>CCTV</h3>
+                     <p>Closed TV circuit. Security for your home, company or business.</p>
+                  </div>
+               </dir>
+               <dir class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="for_box">
+                     <i><img src="icon/4.png"/></i>
+                     <h3>INSTALLATION OF NETWORKS.</h3>
+                     <p>Installations and structured cabling for voice and data networks with implementations of telecommunications rooms (SITE).</p>
+                  </div>
+               </dir>
+            </div>
+         </div>
+       </div>
+      </div>
+<!-- end CHOOSE -->
+
+      
+
+      
+
+         
+      </div>
+      </footr>
+      <!-- Javascript files--> 
+      <script src="js/jquery.min.js"></script> 
+      <script src="js/popper.min.js"></script> 
+      <script src="js/bootstrap.bundle.min.js"></script> 
+      <script src="js/jquery-3.0.0.min.js"></script> 
+      <script src="js/plugin.js"></script> 
+      <!-- sidebar --> 
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script> 
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         
+         $(".zoom").hover(function(){
+         
+         $(this).addClass('transition');
+         }, function(){
+         
+         $(this).removeClass('transition');
+         });
+         });
+         
+      </script> 
+   </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Netline</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+      <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+      <!-- style css -->
+      <!-- <link rel="stylesheet" href="css/style.css"> -->
+      <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+      <!-- Responsive-->
+      <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+      <link href="{{ asset('responsive.css') }}" rel="stylesheet">
+      <!-- fevicon -->
+      <!-- <link rel="icon" href="images/fevicon.png" type="image/gif" /> -->
+      <link href="{{ asset('images/fevicon.png') }}" rel="stylesheet">
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader --> 
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="head_top">
+               <div class="container">
+                  <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <ul class="sociel_link">
+                         <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-linkedin"></i></a></li>
+                     </ul>
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <p>long established fact that a reader will be </p>
+                    </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="index.html"><img src="images/logo.PNG" alt="logo"/></a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-7 col-lg-7 col-md-9 col-sm-9">
+                  <div class="menu-area">
+                     <div class="limit-box">
+                        <nav class="main-menu">
+                           <ul class="menu-area-main">
+                              <li class="active"> <a href="index.html">Home</a> </li>
+                              <li> <a href="about.html">About us</a> </li>
+                              <li> <a href="product.html">packages</a> </li>
+                              <li> <a href="blog.html"> solution</a> </li>
+                              <li> <a href="contact.html">Contact</a> </li>
+                              <li class="mean-last"> <a href="#contact">signup</a> </li>
+                               
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                  <li><a class="buy" href="#">Login</a></li>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner --> 
+      </header>
+      <!-- end header -->
+      <section class="slider_section">
+         <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
+
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="first-slide" src="images/net 2.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>network <br> <strong class="black_bold">solution </strong><br>
+                           <strong class="yellow_bold">SOLUTION </strong></h1>
+                        <p>structured cabling Wi-Fi  <br>
+                           installations routers and switches </p>
+                        <a  href="#">product information</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="second-slide" src="images/net 3.jpg" alt="Second slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1><br><strong class="black_bold">connect!</strong><br>
+                           <strong class="yellow_bold"></strong></h1>
+                        <p>with our technology<br>
+                           you can be in connection with everyone</p>
+                        <a  href="#">see more Products</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="third-slide" src="images/net 4.jpg" alt="Third slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>telephony <br> <strong class="black_bold"> </strong><br>
+                           <strong class="yellow_bold">Product </strong></h1>
+                        <p>we solve all kinds of needs</p>
+                        <a  href="#">see more Products</a>
+                     </div>
+                  </div>
+               </div>
+
+            </div>
+            <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+            <i class='fa fa-angle-right'></i>
+            </a>
+            <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+            <i class='fa fa-angle-left'></i>
+            </a>
+            
+         </div>
+
+      </section>
+
+
+
+<!-- CHOOSE  -->
+      <div class="whyschose">
+         <div class="container">
+
+            <div class="row">
+               <div class="col-md-7 offset-md-3">
+                  <div class="title">
+                     <h2>Why <strong class="black">choose us</strong></h2>
+                     <span>Structured Cabling in Guadalajara We provide professional service and personalized attention</span>
+                     <h3>--------------------------------</h3>
+                     <h2>we<strong class="black"> focus </strong></h2>
+                     <span>We are a company focused on the Information Technology sector, offering retail services for electronic products such as networks, structured cabling, CCTV, etc. as well as installation, maintenance and revision services of the same approach.</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+<!-- end CHOOSE -->
+
+      <!-- service --> 
+      <div class="service">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-8 offset-md-2">
+                  <div class="title">
+                     <h2>Service <strong class="black">Process</strong></h2>
+                     <span>Easy and effective way to get your device repair</span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service1.png"/></i>
+                     <h3>Fast service</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service2.png"/></i>
+                     <h3>Secure payments</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service3.png"/></i>
+                     <h3>Expert team</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service4.png"/></i>
+                     <h3>Affordable services</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service5.png"/></i>
+                     <h3>90 Days warranty</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="service-box">
+                     <i><img src="icon/service6.png"/></i>
+                     <h3>Award winning</h3>
+                     <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end service -->
+
+
+              
+              
+              
+               
+               </div>
+            </div>
+         </div>
+      <!-- end our product -->
+      <!-- map -->
+      <div class="container-fluid padi">
+         <div class="map">
+            <img src="images/mapimg.jpg" alt="img"/>
+         </div>
+      </div>
+      <!-- end map --> 
+      <!--  footer --> 
+
+
+
+
+ 
+      <!-- Javascript files--> 
+      <script src="js/jquery.min.js"></script> 
+      <script src="js/popper.min.js"></script> 
+      <script src="js/bootstrap.bundle.min.js"></script> 
+      <script src="js/jquery-3.0.0.min.js"></script> 
+      <script src="js/plugin.js"></script> 
+      <!-- sidebar --> 
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script> 
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         
+         $(".zoom").hover(function(){
+         
+         $(this).addClass('transition');
+         }, function(){
+         
+         $(this).removeClass('transition');
+         });
+         });
+         
+      </script> 
+   </body>
+</html>
